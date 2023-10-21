1999a1fbf32a7db7b86a87c9cae9e828a9295a65
hangnik <gkdud667485@naver.com>
2023-10-21 23:21:46 +0900

    📝 #30 글로벌스타일 및 기본레이아웃 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import BasicLayout from "./styles/BasicLayout";
+import GlobalStyle from "./styles/GlobalStyle";
+import Theme from "./styles/Theme";
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <BasicLayout>여기에 컴포넌트 넣어서 테스트하면 됩니다</BasicLayout>
+      </ThemeProvider>
+    </>
   );
 }
 export default App;
---
+++ src/styles/Theme.jsx
@@ -0,0 +1,22 @@
+export const colors = {
+  mainColor: "#40A6DE",
+  disabledColor: "#9ACEF8",
+  placeHolderColor: "#DBDBDB",
+  textColor: "#767676",
+  blackColor: "#000000",
+  alertColor: "#EB5757",
+};
+
+export const fontSize = {
+  xsmall: "0.625rem",
+  small: " 0.6875rem",
+  medium: "0.875rem",
+  large: "1.5rem",
+};
+
+const Theme = {
+  colors,
+  fontSize,
+};
+
+export default Theme;
