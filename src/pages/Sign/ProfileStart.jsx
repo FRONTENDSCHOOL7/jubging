971680e95bd25fc6b7bbdc4cae70cb59838231f4
kkang123 <one98kr@gmail.com>
2023-10-22 22:51:58 +0900

    ✨ #29 상단탭, 버튼 추가

--- src/pages/Sign/ProfileStart.jsx
+++ src/pages/Sign/ProfileStart.jsx
@@ -3,24 +3,30 @@ import styled from "styled-components";
 import UserProfile from "../../components/common/Profile/ProfileImage";
 import Input from "../../components/common/Input/Input";
 import { Title, Form, Selfchange } from "./ProfileStartStyle";
+import Button from "../../components/common/Button/ButtonContainer";
+import BackSpaceHeader from "../../components/common/Header/BackSpaceHeader";
 
 const ProfileStartPage = () => {
   const [selfIntroduction, setUserSelf] = useState("");
 
   return (
-    <Form>
-      <Title>프로필 설정</Title>
-      <Selfchange>나중에 언제든지 변경할 수 있습니다.</Selfchange>
-      <UserProfile />
-      <Input
-        label="소개"
-        type="text"
-        name="selfIntroduction"
-        value={selfIntroduction}
-        onChange={(e) => setUserSelf(e.target.value)}
-        placeholder="한 줄 소개를 입력해주세요."
-      />
-    </Form>
+    <>
+      <BackSpaceHeader />
+      <Form>
+        <Title>프로필 설정</Title>
+        <Selfchange>나중에 언제든지 변경할 수 있습니다.</Selfchange>
+        <UserProfile />
+        <Input
+          label="소개"
+          type="text"
+          name="selfIntroduction"
+          value={selfIntroduction}
+          onChange={(e) => setUserSelf(e.target.value)}
+          placeholder="한 줄 소개를 입력해주세요."
+        />
+        <Button width="322px">깨끗한 지구 만들러 가기</Button>
+      </Form>
+    </>
   );
 };
 
--- src/pages/Sign/SignUp.jsx
+++ src/pages/Sign/SignUp.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Input from "../../components/common/Input/Input";
 import { Form, Title } from "./SignUpStyle";
+import Button from "../../components/common/Button/ButtonContainer";
+import BackSpaceHeader from "../../components/common/Header/BackSpaceHeader";
 
 const SignUpPage = () => {
   const [userEmail, setUserEmail] = useState("");
@@ -9,45 +11,50 @@ const SignUpPage = () => {
   const [userId, setUserId] = useState("");
 
   return (
-    <Form>
-      <Title>이메일 회원가입</Title>
-      <Input
-        label="이메일"
-        type="text"
-        name="userEmail"
-        value={userEmail}
-        onChange={(e) => setUserEmail(e.target.value)}
-        placeholder="이메일를 입력하세요."
-        // error="이미 있는 이메일입니다."
-      />
-      <Input
-        label="비밀번호"
-        type="password"
-        name="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="비밀번호를 입력하세요."
-        // error="이미 있는 비밀번호입니다."
-      />
-      <Input
-        label="이름"
-        type="text"
-        name="userName"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
-        placeholder="2~10자 이내여야 합니다."
-        // error="이미 있는 이메일입니다."
-      />
-      <Input
-        label="계정ID"
-        type="text"
-        name="userId"
-        value={userName}
-        onChange={(e) => setUserId(e.target.value)}
-        placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
-        // error="이미 있는 이메일입니다."
-      />
-    </Form>
+    <>
+      <BackSpaceHeader />
+      <Form>
+        <Title>이메일 회원가입</Title>
+        <Input
+          label="이메일"
+          type="text"
+          name="userEmail"
+          value={userEmail}
+          onChange={(e) => setUserEmail(e.target.value)}
+          placeholder="이메일를 입력하세요."
+          // error="이미 있는 이메일입니다."
+        />
+        <Input
+          label="비밀번호"
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="비밀번호를 입력하세요."
+          // error="이미 있는 비밀번호입니다."
+        />
+        <Input
+          label="이름"
+          type="text"
+          name="userName"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          placeholder="2~10자 이내여야 합니다."
+          // error="이미 있는 이메일입니다."
+        />
+        <Input
+          label="계정ID"
+          type="text"
+          name="userId"
+          value={userName}
+          onChange={(e) => setUserId(e.target.value)}
+          placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
+          // error="이미 있는 이메일입니다."
+        />
+
+        <Button width="322px">다음</Button>
+      </Form>
+    </>
   );
 };
 
