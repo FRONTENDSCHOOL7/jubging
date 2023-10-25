b96c67d00593457670dd5aca142d4e9357bc52b9
hangnik <gkdud667485@naver.com>
2023-10-25 13:11:04 +0900

    ✨ 홈, 프로필, 채팅 라우터 추가 및 변경

--- src/pages/Follow/Followers.jsx
+++ src/pages/Follow/Followers.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import FollowerHeader from "../../components/common/Header/FollowerHeader";
 import UserListBox from "../../components/common/UserList/UserListBox";
 import ButtonContainer from "../../components/common/Button/ButtonContainer";
+import styled from "styled-components";
+
+const UserContainer = styled.div`
+  margin-top: 25px;
+`;
 
 export default function Followrs() {
   return (
     <>
       <FollowerHeader />
-      <UserListBox>
-        <ButtonContainer width={"65px"} height={"28px"} bgColor={"#40A6DE"}>
-          팔로우
-        </ButtonContainer>
-      </UserListBox>
+      <UserContainer>
+        <UserListBox>
+          <ButtonContainer width={"65px"} height={"28px"} bgColor={"#40A6DE"}>
+            팔로우
+          </ButtonContainer>
+        </UserListBox>
+      </UserContainer>
     </>
   );
 }
--- src/pages/Follow/Following.jsx
+++ src/pages/Follow/Following.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import FollowingHeader from "../../components/common/Header/FollowingHeader";
 import UserListBox from "../../components/common/UserList/UserListBox";
 import ButtonContainer from "../../components/common/Button/ButtonContainer";
+import styled from "styled-components";
+
+const UserContainer = styled.div`
+  margin-top: 25px;
+`;
 
 export default function Following() {
   return (
     <>
       <FollowingHeader />
-      <UserListBox>
-        <ButtonContainer width={"65px"} height={"28px"} bgColor={"#40A6DE"}>
-          팔로우
-        </ButtonContainer>
-      </UserListBox>
+      <UserContainer>
+        <UserListBox>
+          <ButtonContainer width={"65px"} height={"28px"} bgColor={"#40A6DE"}>
+            팔로우
+          </ButtonContainer>
+        </UserListBox>
+      </UserContainer>
     </>
   );
 }
