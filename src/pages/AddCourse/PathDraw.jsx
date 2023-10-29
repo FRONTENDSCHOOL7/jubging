d5437364112a7d324793908dd374fa0b280e59b5
kkang123 <one98kr@gmail.com>
2023-10-29 23:58:02 +0900

    💄 #58 AddCourse, PathDraw 페이지 스타일

--- src/pages/AddCourse/PathDraw.jsx
+++ src/pages/AddCourse/PathDraw.jsx
@@ -1,27 +1,22 @@
 import React from "react";
 import UploadHeader from "../../components/common/Header/UploadHeader";
 import Navbar from "../../components/common/Navbar/Navbar";
-import {
-  Wrapper,
-  Title,
-  MapCanvas
-} from "./PathDrawStyle";
+import { Wrapper, Title, MapCanvas } from "./PathDrawStyle";
 
-const PathDraw = ({nickname}) => {
+const PathDraw = ({ nickname }) => {
   return (
     <>
       <UploadHeader />
       <Wrapper>
         <Title>
           {nickname} 님 만의 <br />
-          플로깅 코스를 입력해주세요.  
+          플로깅 코스를 입력해주세요.
         </Title>
-        <MapCanvas>
-        </MapCanvas>
+        <MapCanvas></MapCanvas>
       </Wrapper>
       <Navbar />
     </>
   );
-}
+};
 
 export default PathDraw;
