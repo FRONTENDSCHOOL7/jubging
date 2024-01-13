0bdaba322fd8264817914ed324d94378e79ab332
hangnik <gkdud667485@naver.com>
2024-01-13 23:18:57 +0900

    🔥 필요없는 코드 및 아이콘 이미지 삭제

--- src/pages/Profile/ProfileEdit.jsx
+++ src/pages/Profile/ProfileEdit.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { BASE_URL } from "../../api/axios";
-import { postImgUpload } from "../../api/imageAPI";
 import { setUserProfile } from "../../api/profileAPI";
 import { checkAccountname } from "../../api/signupAPI";
 
