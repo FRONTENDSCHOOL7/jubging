047c0f4eaa07c46d5bc21ec8c57dfa6630fb4872
hangnik <gkdud667485@naver.com>
2024-01-23 12:54:22 +0900

    🔨 label id값 추가

--- src/components/Chat/ChatInput.jsx
+++ src/components/Chat/ChatInput.jsx
@@ -13,12 +13,14 @@ function ChatInput() {
         <img src={PlusIcon} alt="plus" />
       </Button>
       <RoundInput
-        label=""
+        id="chat"
+        label="채팅 메시지 입력"
         type="text"
         name="message"
         placeholder="메시지 입력하기..."
         value={message}
         onChange={(e) => setMessage(e.target.value)}
+        hidden
       />
       <Button $active={message !== ""}>전송</Button>
     </ChatBar>
